a7470c040a7320684aa76e113379979c6ed35301
MarianGhaly3 <70914022+MarianGhaly3@users.noreply.github.com>
2022-03-05 13:11:21 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -7,6 +7,24 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+//scroll to top button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 //slider automatically function
 var slideIndex = 0;
 showSlides();
